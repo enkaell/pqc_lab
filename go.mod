438c63d7c7e217b0f2941a469c41a88da1057a17
Vladislav Chesnokov <Vladislav.Chesnokov@6ghi.net>
2024-11-05 15:39:04 +0100

    protobuf file init

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module pqc_lab/mod
 
 go 1.21.4
 
-require github.com/google/uuid v1.6.0 // indirect
+require github.com/google/uuid v1.6.0
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+)
--- internals/models/models.go
+++ internals/models/models.go
@@ -8,10 +8,19 @@ type Algorithm struct {
 	AlgorithmId   uuid.UUID
 	AlgorithmName string
 }
-
-type AlgorithmRun struct {
-	AlgorithmId    uuid.UUID
-	AlgorithmInfo1 string
-	AlgorithmInfo2 string
-	AlgorithmInfo3 string
+type AlgorithmVersion struct {
+    AlgorithmVersionId  uuid.UUID
+    AlgorithmId  uuid.UUID
+    AlgorithmVersionName
 }
+type AlgorithmVersionRun struct {
+	AlgorithmVersionRunId  uuid.UUID
+    AlgorithVersionId  uuid.UUID
+	Keygen string
+	Sign string
+	Verify string
+    PrivateKeySize unsigned int32 = 480
+    PublicKeySize unsigned int32 = 288
+    SignatureSize unsigned int32 = 96
+}
+
